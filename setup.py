560a0125f73ed3ffe1f56f737b524eccb58b1c63
GokulRaj S <168752490+Gokulraj0906@users.noreply.github.com>
2025-10-01 13:11:36 +0530

    updated the new line as license='GPL-3.0 license'

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description=open('README.md', encoding='utf-8').read(), 
     long_description_content_type='text/markdown',
     url='https://github.com/gokulraj0906/GQNN',
-    license='MIT',
+    license='GPL-3.0 license',
     packages=find_packages(),
     install_requires=[
         'numpy',
